540648ae316b18920f032ee0165abfded3a70b8c
Anton Brisilin <a.brisilin@innopolis.university>
2021-08-23 00:39:00 +0300

    Used prod Flask server

--- app_python/src/main/main.py
+++ app_python/src/main/main.py
@@ -1,7 +1,13 @@
+"""Entrypoint of application"""
+import os
 from flask import Flask
+from waitress import serve
 
 from src.main import config
 from src.main import routes
 
 app = Flask(config.app_name)
 routes.init_routes(app)
+
+if os.environ['MODE'] == 'PROD':
+    serve(app, port=5000)
