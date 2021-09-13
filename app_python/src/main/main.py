e973434cd0d6058ca0f6d85db0c2f08cae82ae63
Anton Brisilin <a.brisilin@innopolis.university>
2021-09-13 20:23:44 +0300

    Add logs to app

--- app_python/src/main/main.py
+++ app_python/src/main/main.py
@@ -1,7 +1,9 @@
 """Entrypoint of application"""
 import os
+import logging
 from flask import Flask
 from waitress import serve
+from paste.translogger import TransLogger
 
 from src.main import config
 from src.main import routes
@@ -9,5 +11,11 @@ from src.main import routes
 app = Flask(config.app_name)
 routes.init_routes(app)
 
+logging.basicConfig()
+logger_names = ['waitress', 'wsgi']
+loggers = [logging.getLogger(name) for name in logger_names]
+
 if os.environ['MODE'] == 'PROD':
-    serve(app, port=5000)
+    for logger in loggers:
+        logger.setLevel(logging.INFO)
+    serve(TransLogger(app, loggers[1]), port=5000)
